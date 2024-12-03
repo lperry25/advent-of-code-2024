91621ebb7b1149e66d2a61ef7362c48e1fefbb2a
Laura Perry <10005215+lperry25@users.noreply.github.com>
2024-12-03 13:57:10 +0100

    solved day 1 part 1 and 2

--- src/components/DaySelection.tsx
+++ src/components/DaySelection.tsx
@@ -1,9 +1,18 @@
-export function DaySelection({ day, setDay, selectedDay }: 
-    { day: number, setDay: (day: number) => void, selectedDay: number | null }
-){
-    return (
-        <button className={`${day === selectedDay ? 'text-red-500' : 'text-white'} mx-5`} onClick={() => setDay(day)}>
-            {day}
-        </button>
-    )
+export function DaySelection({
+  day,
+  setDay,
+  selectedDay,
+}: {
+  day: number;
+  setDay: (day: number) => void;
+  selectedDay: number | null;
+}) {
+  return (
+    <button
+      className={`${day === selectedDay ? 'text-red-500' : 'text-white'} mx-5`}
+      onClick={() => setDay(day)}
+    >
+      {day}
+    </button>
+  );
 }
---
+++ src/components/days/day1.tsx
@@ -0,0 +1,36 @@
+import { Day } from '../Day';
+
+export function Day1() {
+  const splitLists = (input: string) => {
+    let leftColumn = [];
+    let rightColumn = [];
+
+    // Split the data by lines and parse each line
+    input
+      .trim()
+      .split('\n')
+      .forEach((line) => {
+        const [left, right] = line.trim().split(/\s+/).map(Number);
+        leftColumn.push(left);
+        rightColumn.push(right);
+      });
+
+    console.log('Left Column:', leftColumn.sort());
+    console.log('Right Column:', rightColumn.sort());
+    return [leftColumn.sort(), rightColumn.sort()];
+  };
+
+  const solve1 = (input: string) => {
+    const [left, right] = splitLists(input);
+    const differenceSum = left.reduce((sum, leftNum, i) => sum + Math.abs(right[i] - leftNum), 0)
+    return differenceSum
+  };
+
+  const solve2 = (input: string) => {
+    
+    const [left, right] = splitLists(input);
+    const result = left.reduce((sum, leftNum, i) => sum + leftNum * right.filter(num => num === leftNum).length, 0)
+    return result
+  };
+  return <Day solve1={solve1} solve2={solve2} />;
+}
